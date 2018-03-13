169206c7a86ea64f73e2d51f63a3fc88a28e01aa
Maxim Mosyura <maxim.mosyura@dataart.com>
2018-03-13 14:48:05 +0200

    add User class and seneca microservice for user

--- serverSide/actions.js
+++ serverSide/actions.js
@@ -1,5 +1,7 @@
 const inventory = require('./services/inventory');
+const user = require('./services/user');
 
 module.exports = function actions (options) {
     this.add({ role:'inventory', cmd:'find_item' }, inventory.add);
+    this.add({ cmd:'user' }, user);
 };
